887cf43ff604f9244bd557a72e1248783f2060e9
reapertheone <tutrai.gergo01@gmail.com>
2021-02-06 20:18:06 +0000

    tuptup

--- index.js
+++ index.js
@@ -1,16 +1,41 @@
 const { app, BrowserWindow, ipcMain, Notification}=require('electron')
 const path=require('path')
+const express=require('express')
+const expressApp=express()
+
+let mainWindow;
+let settingsWindow
 
 app.on('ready',()=>{
-    mainWindow=new BrowserWindow({
+        mainWindow=new BrowserWindow({
         webPreferences:{nodeIntegration:true},
         fullscreen:true,
         backgroundColor:'#383838'
     })
 
     mainWindowUrl=path.join(__dirname,'./views/main.html')
-    mainWindow.loadURL(mainWindowUrl)
     
+    expressApp.listen(3000,()=>{
+        console.log('Listening on port 3000')
+    })
+    mainWindow.loadURL('http://localhost:3000')
     
+})
 
+ipcMain.on('settings:open',()=>{
+    settingsWindow=new BrowserWindow({
+        webPreferences:{nodeIntegration:true}
+    })
+    
+    settingsWindow.loadURL(path.join(__dirname,'./views/settings.html'))
+})
+
+expressApp.set('views',path.join(__dirname,'./views'))
+
+expressApp.get('/',(req,res)=>{
+    res.sendFile(path.join(__dirname,'./views/main.html'))
+})
+
+expressApp.get('/settings',(req,res)=>{
+    res.sendFile(path.join(__dirname,'./views/settings.html'))
 })
